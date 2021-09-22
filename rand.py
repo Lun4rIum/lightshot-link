f51d3cdf5afa51da39a9bc881ff63b0e9df152b6
Lun4rIum <67378940+Lun4rIum@users.noreply.github.com>
2021-09-22 12:09:53 +0200

    Add files via upload

---
+++ rand.py
@@ -0,0 +1,42 @@
+import itertools
+from bs4 import BeautifulSoup
+import requests
+import lxml
+import random
+
+ls = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"]
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+while True:
+
+    
+  
+    url = r"https://prnt.sc/" + ''.join(random.choices(ls, k=5))
+   
+    req = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(req.text, "html.parser")
+    x=soup.find_all('img')
+ 
+    ss = '//st.prntscr.com/2021/04/08/1538/img/0_173a7b_211be8ff.png' in str(x)
+ 
+
+    if  ss == False:
+        
+
+        file=open("images.txt","r")
+        contenu=" ".join(file.read().split('\n')).split(" ")
+        file.close()
+
+        if url not in contenu:
+
+            f = open('images.txt', "a")
+            f.write(url)
+            f.write('\n')     
+            f.close()
+            print(url)   
+
+
+          
+
