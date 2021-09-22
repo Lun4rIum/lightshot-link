f6422142d30ea2295e6b1d55787b5bb5b2569664
Lun4rIum <67378940+Lun4rIum@users.noreply.github.com>
2021-09-22 12:10:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
 from bs4 import BeautifulSoup
 import requests
-import lxml
 import random
 
 ls = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"]
